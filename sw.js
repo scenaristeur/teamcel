c220410bde7a5a29334ec2f448c7325ab0299929
Scenaristeur <scenaristeur@gmail.com>
2026-07-25 19:01:55 +0200

    change room pwa share

---
+++ sw.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'teamcel-v1';
+var urlsToCache = [
+    './',
+    './index.html',
+    './main.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(name) { return name !== CACHE_NAME; })
+                    .map(function(name) { return caches.delete(name); })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            return caches.match(event.request);
+        })
+    );
+});
